63c29656722253b63341edfe1231e7bf19b0f7da
Aadharbrahmbhatt <138867589+Aadharbrahmbhatt@users.noreply.github.com>
2024-11-24 15:48:00 +0530

    Add files via upload

---
+++ maxlength.js
@@ -0,0 +1,8 @@
+function mode(){
+    let btn1 = document.getElementById("btn1")
+    if(btn1.checked){
+    document.body.childNodes[1].style.background = "white";
+    document.body.childNodes[1].style.text = "black";
+    alert()
+    }
+}
